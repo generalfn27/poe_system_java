5f437149ea8cc0afbb8473008fe20deb04fa9133
Felix <generalfelix27@gmail.com>
2024-09-01 16:45:34 +0800

    andito explanation bakit hanggang dito palang muna

--- src/shopper/customer.java
+++ src/shopper/customer.java
@@ -1,5 +1,17 @@
 package shopper;
 
 public class customer {
-    //private String
+    //private String kasi dito dba sa customer may registered at may guest
+    // so di ko pa alam ano lalagay kong attributes dito sa customer
+    // kasi every customer may name sila tas chuchu
+    // pero nag sstart muna sa guest ba sila or registered
+
+    //pag guest user walang login so walang ipapasa na value dito sa constructor na gagawin
+    //tapos pag registered may account number at account id nalang
+
+    //kada id/account details kaya di ko pa maisip anong ipapasa
+
+    // or gagawa muna ng another customer type na function bale boolean if registered customer
+
+    //tapos ibang function nalng ulit kgaya nung asa C program
 }
