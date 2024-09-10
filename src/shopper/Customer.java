eeb907da40b244cd4db855ae24a014e691e3b14f
Felix <generalfelix27@gmail.com>
2024-09-11 03:57:42 +0800

    class edited

--- src/shopper/Customer.java
+++ src/shopper/Customer.java
@@ -1,13 +1,13 @@
 package shopper;
 
-public class customer {
+public class Customer {
     private String name;
     private boolean isRegistered;
     private String accountNumber;
     private String accountId;
 
     // Constructor/function for guest customers (no account details)
-    public customer(String name) {
+    public Customer(String name) {
         this.name = name;
         this.isRegistered = false; // Guests are not registered by default
         this.accountNumber = null;
@@ -15,7 +15,7 @@ public class customer {
     }
 
     // Constructor for registered customers (with account details)
-    public customer(String name, String accountNumber, String accountId) {
+    public Customer(String name, String accountNumber, String accountId) {
         this.name = name;
         this.isRegistered = true; // Registered customer
         this.accountNumber = accountNumber;
