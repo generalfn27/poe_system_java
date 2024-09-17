6ab4a6754035106be422c99077e9f42e7667ef84
Felix <generalfelix27@gmail.com>
2024-09-17 19:11:22 +0800

    KASAMA ANG PAG CHECK NG QUEUE NUMBER EVERY RUN

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,12 +2,15 @@ package Main;
 
 import User_Types.UserType;
 
+import static Process.OrderProcessor.initialize_queue_number;
+
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
 
         UserType user_type = new UserType();
         user_type.user_type_menu();
+        initialize_queue_number();
 
         //System.out.flush();
     }
