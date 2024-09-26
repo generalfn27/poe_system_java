76188e6e98a28e8bd391410e922efea9c38eb60c
Felix <generalfelix27@gmail.com>
2024-09-27 07:12:16 +0800

    naka static na mga menu

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,8 +8,7 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
         initialize_queue_number();
-        UserType user_type = new UserType();
-        user_type.user_type_menu();
+        UserType.user_type_menu();
 
         System.out.flush();
     }
