fc17cba4df6a90a27174cf6db469f84c6058d646
Felix <generalfelix27@gmail.com>
2024-09-28 19:48:14 +0800

    new import para sa static method para mag check ng queue number kada start ng program

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,13 +1,12 @@
 package Main;
 
 import User_Types.UserType;
-
-import static Process.OrderProcessor.initialize_queue_number;
+import Process.OrderProcessor;
 
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
-        initialize_queue_number();
+        OrderProcessor.initialize_queue_number();
         UserType.user_type_menu();
 
         System.out.flush();
