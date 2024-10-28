7274106c96f716500a6434421d0576e29e64c885
Felix <generalfelix27@gmail.com>
2024-10-28 23:34:39 +0800

    getters and setters palang tapos nxt update ay pwede na gumawa ng accounts

--- src/Employee/Cashier.java
+++ src/Employee/Cashier.java
@@ -12,6 +12,34 @@ import java.util.Scanner;
 public class Cashier {
     private static final int MAX_ATTEMPTS = 3; // Maximum login attempts
     private final CashierProcess cashier_process; // Class-level instance variable for CashierProcess
+    private String employee_id;
+    private String employee_name;
+    private String password;
+
+    public String getEmployee_id() {
+        return employee_id;
+    }
+
+    public void setEmployee_id(String employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public String getEmployee_name() {
+        return employee_name;
+    }
+
+    public void setEmployee_name(String employee_name) {
+        this.employee_name = employee_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword (String password) {
+        this.password = password;
+    }
+
 
     public Cashier() {
         this.cashier_process = new CashierProcess(); // Assign to the class-level variable
