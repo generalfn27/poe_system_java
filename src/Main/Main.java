fe26ab7ca292daa9077d375ef6124921dd458d43
Felix <generalfelix27@gmail.com>
2024-11-04 18:24:37 +0800

    clear

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,12 +6,9 @@ import Process.CashierProcess;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Hello world!");
         OrderProcessor.initialize_queue_number();
         CashierProcess.initialize_receipt_number();
 
         UserType.user_type_menu();
-
-        System.out.flush();
     }
 }
