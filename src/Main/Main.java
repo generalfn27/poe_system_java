ccc163f6f72dfaf5a6c914248820d7fd4a03107c
Felix <65657867+generalfn27@users.noreply.github.com>
2024-11-14 17:28:26 +0800

    Update Main.java
    note ano need baguhin

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,6 +9,8 @@ public class Main {
         OrderProcessor.initialize_queue_number();
         CashierProcess.initialize_receipt_number();
 
+//naka if no tong part na to at throwing boolean 
+//dahil sa first open ng program dapat may store name setup at manager setup
         UserType.user_type_menu();
     }
 }
