a3404bfb8f90db505e7887f8b07316c9e44b8249
Felix <generalfelix27@gmail.com>
2024-12-01 11:40:38 +0800

    sa pinaka una may setup storename at manager na

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,14 +3,17 @@ package Main;
 import User_Types.UserType;
 import Process.OrderProcessor;
 import Process.CashierProcess;
+import Employee.ManagerCredentials;
 
 public class Main {
     public static void main(String[] args) {
         OrderProcessor.initialize_queue_number();
         CashierProcess.initialize_receipt_number();
 
-    //naka if no tong part na to at throwing boolean 
-    //dahil sa first open ng program dapat may store name setup at manager setup
+        if (ManagerCredentials.needs_initial_setup()) {
+            ManagerCredentials.initialSetup();
+        }
         UserType.user_type_menu();
     }
+
 }
