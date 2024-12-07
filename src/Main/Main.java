103b00f579038c84d1c7ce6c76c3ebbd41d9daf4
Felix <generalfelix27@gmail.com>
2024-12-07 18:00:28 +0800

    wala ng initialization kasi pampa complex lang

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,15 +1,10 @@
 package Main;
 
 import User_Types.UserType;
-import Process.OrderProcessor;
-import Process.CashierProcess;
 import Employee.ManagerCredentials;
 
 public class Main {
     public static void main(String[] args) {
-        OrderProcessor.initialize_queue_number();
-        CashierProcess.initialize_receipt_number();
-
         if (ManagerCredentials.needs_initial_setup()) {
             ManagerCredentials.initialSetup();
         }
