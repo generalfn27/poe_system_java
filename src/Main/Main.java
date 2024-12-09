210fa23199318a24131c2d850a6058493b52ab74
Felix <generalfelix27@gmail.com>
2024-12-09 20:43:16 +0800

    dinagdag lng ung para mag iba kulay ng console

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,6 +5,7 @@ import Employee.ManagerCredentials;
 
 public class Main {
     public static void main(String[] args) {
+        //System.out.print("\033[48;5;15m\033[30m");  // White background and black text
         if (ManagerCredentials.needs_initial_setup()) {
             ManagerCredentials.initialSetup();
         }
